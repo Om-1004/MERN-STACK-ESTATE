8342b852b286a0c7fce0a561bf090698cad08ab7
Om-1004 <opatel101004@gmail.com>
2024-12-20 17:37:16 -0500

    Protected Profile Route and Updated Header UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,10 @@
 import { Search } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
+
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center border-2 max-w-6xl mx-auto p-3">
@@ -29,10 +33,16 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to="/sign-in">
-            <li className="sm:inline text-slate-700 hover:underline">
-              Sign In
-            </li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
+            ) : (
+              <li className=" text-slate-700 hover:underline"> Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
